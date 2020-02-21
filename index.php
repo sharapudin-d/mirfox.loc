e6df0ad8304a8fa35ec9f6213e4a5958da4330b3
sharapudin-d <32812558+sharapudin-d@users.noreply.github.com>
2020-02-21 21:14:37 +0300

    Sample of project for Xakathon
    MirFox

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MirFox</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+<body>
+    <h1>How is world?</h1>
+    <?php echo "Hello World"?>
+    <script src="./assets/js/script.js"></script>
+</body>
+</html>
